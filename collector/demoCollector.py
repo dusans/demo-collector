ce04876f121d55bf67f638a0a11787bf2c886245
dusan.smitran <dusan.smitran@c93da026-f340-11de-8355-bf90974c4141>
2010-01-02 11:48:57 +0000

    *build-27:*  * Add: Refactoring code and gui  * Add: Linux and Windows command line support

--- collector/demoCollector.py
+++ collector/demoCollector.py
@@ -1,7 +1,10 @@
 import os
+import sys
 import re
 import shutil
 import datetime
+import yaml
+from yaml.parser import ParserError
 
 class Demo:
     def __init__(self, path, screenShots=[]):
@@ -32,7 +35,7 @@ class ScreenShot:
         return "<ScreenShot: name: %s, date: %s>" % (self.name, self.date)
 
 class Game:
-    def __init__(self, name, demoFolder, screenShotFolder, outputFolder, gameDemoExt, nameShorteners=[], grouper=[""]):
+    def __init__(self, name, demoFolder, screenShotFolder, outputFolder, gameDemoExt, nameShorteners={}, grouper=[""]):
         self.name = name
         self.demoFolder = demoFolder
         self.screenShotFolder = screenShotFolder
@@ -59,11 +62,33 @@ class Game:
                                             self.outputFolder,
                                             self.gameDemoExt)
 
+    def testRegEx(self, regex):
+        try:
+            re.compile(regex)
+            return True
+        except:
+            return False
+
+    # TODO put this tests into a Settings class
     # Test if all folders exist
     def test(self):
+        # 1. Test folders...
         for folder in [self.demoFolder, self.screenShotFolder, self.outputFolder]:
             if not os.path.isdir(folder):
                 return (False, "%s doesnt exist" % folder)
+        # 2. demo extension
+        if self.gameDemoExt == None:
+            return (False, "U need to  write the extension of the demos: gameDemoExt" )
+        # 3. game name
+        if self.name == None:
+            return (False, "Write the name of the game... Cmon..." )
+        # 4. nameShorteners
+        for s in self.nameShorteners:
+            if not self.testRegEx(self.nameShorteners[s]):
+                return (False, "One of the name shorteners is an invalid regex: %s" % s)
+        # 5. grouper
+        # TODO1 make grouper check... if structure ok and regex fine
+
         return (True, "OK")
 
     #=============== COUNTER ===============
@@ -129,9 +154,9 @@ class Game:
                     print "Error at makedirs... dir maybe already exists"
 
                 # SHORT DEMO NAME, BASSED ON REGEX IN nameShorteners
-                if len(self.nameShorteners) > 0:
-                    for r in self.nameShorteners.values():
-                        name = re.sub(r, "", name)
+                for r in self.nameShorteners.values():
+                    name = re.sub(r, "", name)
+
                 # Copy screenShots
                 for s in d.screenShots:
                     #print "Copy", s
@@ -141,10 +166,98 @@ class Game:
                     deltas.append(delta.strftime("%M.%S"))
                 # Copy demo
                 yield d
+                deltas = set(deltas) # Someone might go crazy and press it to many times in one second :)
                 shutil.copyfile(d.path, "%s/%s/%s-%s__%s__.%s" % (self.outputFolder, groupDir, self.counter, name, "_".join(deltas), d.ext))
                 self.counter += 1
         # After copy set counter and date files
         self.setCounterAndDate()
+
+class Message:
+    def __init__(self, messageType="OK", text=""):
+        self.text = text
+        self.messageType = messageType
+
+    def __str__(self):
+        return "[%s]\t%s" % (self.messageType, self.text)
+
+# This is the default output of text and messages for the Collector
+class Output:
+    def __init__(self):
+        pass
+    def write(self, message):
+        print message
+
+class Settings:
+    def __init__(self, settingsFile):
+        self.settingsFile = settingsFile
+    # ==== LOAD SETTINGS ====
+    def load(self):
+        self.settings = {}
+
+        try:
+            self.settings = yaml.load(self.settingsFile)
+            return Message("OK", "YAML Settings loadet successfully")
+        except IOError, e:
+            return Message("Error", "Settings file not found! There should be a settings.yml in files directory!")
+        except ParserError, e:
+            return Message("Error", "Settings load ERROR. YAML setting file is corrupt!\n %s" % str(e))
+
+class Collector:
+
+    # settingsFile = open file instance
+    def __init__(self, settingsFile, output=Output()):
+        self.games = []
+        # ==== ==== ==== ====
+        # OUTPUT connectors
+        # ==== ==== ==== ====
+        self.output = output
+        self.demoCountOutput = sys.stdout.write
+        # ==== ==== ==== ====
+        # LOAD SETTINGS
+        # ==== ==== ==== ====
+        self.settingsObject = Settings(settingsFile)
+        self.write(self.settingsObject.load())
+
+        # ==== ==== ==== ====
+        # LOAD GAMES
+        # ==== ==== ==== ====
+        for setting in self.settingsObject.settings['games']:
+            try:
+                self.games.append(Game(**setting))
+            except:
+                self.write(Message("Error", "Loading game settings ERROR. Check settings file"))
+                self.write(Message("Warning", "    error at game: %s" % setting['name']))
+
+    # ==== ==== ==== ====
+    # Write to specified output
+    # ==== ==== ==== ====
+    # output should be an object that has a write method that specifies where to print some text :)
+    def write(self, text):
+        self.output.write(text)
+
+    def collect(self):
+        allDemosNum = 0
+        # ==== ==== ==== ====
+        # All GAMES
+        # ==== ==== ==== ====
+        for game in self.games:
+
+            # ==== Test GAME ====
+            if game.test()[0]:
+                self.write(Message("OK", "Loading game: %s ... game is valid" % game.name))
+
+                # ==== ==== ==== ====
+                # GET DEMOS
+                # ==== ==== ==== ====
+                for demo in game.copy():
+                    self.write(Message("Add", "     copy demo: %s" % demo.name))
+                    # Demo Count LCD
+                    allDemosNum += 1
+                    self.demoCountOutput(str(allDemosNum))
+                    #self.lcdDemoNumber.display(allDemosNum)
+            else:
+                self.write(Message("Warning", "Loading game: %s ... game is invalid!" % game.name))
+                self.write(Message("Warning", "    error: %s" % game.test()[1]))
 
 #-------------------------------------------------------------------------------
 # TEST
@@ -169,3 +282,11 @@ class Game:
 ##                #print "Match", grouper[folder]['v']
 ##                return folder + "/" + group(grouper[folder]['c'], name)
 ##    return ""
+
+# ==== ==== ==== ====
+# MAIN
+# ==== ==== ==== ====
+if __name__ == '__main__':
+    #s = Settings(open("d:/dev/DemoCollector/code/files/settings.yml"))
+    collector = Collector(open("../files/settings.yml"))
+    collector.collect()
